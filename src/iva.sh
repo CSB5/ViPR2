9896eb1327212d671ce79d6a891fd00d81ed2926
Andreas WILM -CSB5 <wilma@gis.a-star.edu.sg>
2016-06-29 16:44:20 +0800

    Changed PATH setup in src/iva.sh which was magically messed up by nested conda envs

--- src/iva.sh
+++ src/iva.sh
@@ -15,15 +15,21 @@ test -d $OUTD && rm -rf $OUTD
 test -e "$FQ1" || exit 1
 test -e "$FQ2" || exit 1
 
+# WARN: we've seen weird cases where nested envs and the path meddling here
+# completely messes things up
+
+
+export PATH=/mnt/software/unstowable/anaconda/bin:$PATH
+#echo "DEBUG: path before source activate iva: $PATH" 1>&2
+source activate iva || echo "Couldn't source IVA" 1>&2
+
+# source deactivate assumes PATH was prepended. the following will mess with the assumption
+
 export PATH=/mnt/software/stow/KMC-2.2/bin/:$PATH
 export PATH=/mnt/software/stow/smalt-0.7.6/bin/:$PATH
 export PATH=/mnt/software/stow/samtools-0.1.19/bin/:$PATH
-export PATH=/mnt/software/unstowable/anaconda/bin/:$PATH
-
 export PERL5LIB=/mnt/software/lib:"/mnt/software/lib/perl5/`perl -e 'printf "%vd", $^V;'`":/mnt/software/share/perl5:$PERL5LLIB
 export PATH=/mnt/software/unstowable/mummer-3.23:$PATH
 
-export PATH=/mnt/software/unstowable/anaconda/bin:$PATH
-
-source activate iva || echo "Couldn't source IVA" 1>&2
 iva -f $FQ1 -r $FQ2 -t $THREADS $OUTD
+echo "DEBUG: path before source activate iva: $PATH" 1>&2
